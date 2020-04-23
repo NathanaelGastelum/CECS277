d6ba5ac351bd341d52d9119bd5ef5fcffe13dfa8
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-04-23 10:00:14 -0700

    decorator pattern lab

---
+++ DecoratorLab/src/Vanilla.java
@@ -0,0 +1,11 @@
+public class Vanilla  extends Dessert{
+    public static final double COST = 1.25;
+
+    public Vanilla() {
+        description = "Vanilla";
+    }
+
+    public double cost() {
+        return COST;
+    }
+}
